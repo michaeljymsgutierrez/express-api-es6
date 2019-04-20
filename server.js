b87684e3f3c11a2e4cdd554ed0b92fc6adff89db
michaeljymsgutierrrez <leeez711@gmail.com>
2019-04-20 11:26:54 +0800

    Updated main server file for node start

--- server.js
+++ server.js
@@ -2,19 +2,39 @@
  * Main Express Server
  */
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
 import morgan from "morgan";
+import ip from "ip";
+import https  from "https";
 import { API } from "./endpoints/test";
+import ENV from "./env";
 
 const app = express();
+const path = __dirname;
+const publicFolder = path + "/files";
 
+/**
+ * Middlewares
+ */
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
 app.use(morgan("dev"));
+app.use("/public", express.static(publicFolder));
 
 app.get("/", (req, res) => {
-  res.json({ success: 200 });
+	res.json({ success: 200 });
 });
 
 app.get("/api", API);
 
-app.listen("3000", "127.0.0.1", () => {
-  console.log("STATUS::SUCCESS");
-});
+
+if(ENV.mode && ENV.mode === "development"){
+	app.listen(ENV.port, ENV.host, () => {
+		console.info(`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${ENV.host}:${ENV.port}\n  =====================================`);
+	});
+}
+
