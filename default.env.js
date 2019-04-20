1bcbbd840f5f759aa620ba4025cfcc11d23c2bf3
michaeljymsgutierrrez <leeez711@gmail.com>
2019-04-20 11:03:38 +0800

    Added default environment script

---
+++ default.env.js
@@ -0,0 +1,32 @@
+/**
+ * Default Environment Configuration
+ */
+
+import ip from "ip";
+
+let ENV = {
+	mode: "development", 
+	port: "3000",
+	host: "127.0.0.1",
+	ssl_directory: null
+};
+
+
+/**
+ * Override values here if necessary
+ */
+if(ENV.mode === "development"){
+	ENV.port = "3000";
+	ENV.host = "127.0.0.1";
+} else if(ENV.mode === "production"){
+	ENV.port = "8080";
+	ENV.host = ip.address();
+	/**
+	 * SSL directory is where you ssl key and cert pem lives
+	 */
+	ENV.ssl_directory: null
+}
+
+
+export default ENV;
+
