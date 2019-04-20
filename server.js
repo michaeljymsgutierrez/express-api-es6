61138ae04f7f793a8a817544f7b2c5b40400cd10
michaeljymsgutierrrez <leeez711@gmail.com>
2019-04-20 11:50:22 +0800

    Added production configuration

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import ip from "ip";
-import https  from "https";
+import https from "https";
+import fs from "fs";
 import { API } from "./endpoints/test";
 import ENV from "./env";
 
@@ -31,10 +32,28 @@ app.get("/", (req, res) => {
 
 app.get("/api", API);
 
-
-if(ENV.mode && ENV.mode === "development"){
+if (ENV.mode && ENV.mode === "development") {
 	app.listen(ENV.port, ENV.host, () => {
-		console.info(`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${ENV.host}:${ENV.port}\n  =====================================`);
+		console.log(
+			`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${
+				ENV.host
+			}:${ENV.port}\n  =====================================`
+		);
+	});
+} else if (ENV.mode === "production") {
+	/**
+	 * SSL Directory for production
+	 */
+	const sslOptions = {
+		key: fs.readFileSync(`${ENV.ssl_directory}/key.pem`),
+		cert: fs.readFileSync(`${ENV.ssl_directory}/cert.pem`)
+	};
+
+	https.createServer(sslOptions, app).listen(ENV.port, ENV.host, () => {
+		console.log(
+			`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${
+				ENV.host
+			}:${ENV.port}\n  =====================================`
+		);
 	});
 }
-
