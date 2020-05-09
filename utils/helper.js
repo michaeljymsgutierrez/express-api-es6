5f9122fd2b8b7ecd396ba16386472b0df03f84b0
michaeljymsgutierrez <leeez711@gmail.com>
2020-05-09 22:23:10 +0800

    Uppercased helper functions

--- utils/helper.js
+++ utils/helper.js
@@ -2,13 +2,13 @@
 // Contains string and formatting functions
 
 class Helper {
-  validate_email(email) {
+  VALIDATE_EMAIL(email) {
     // Validate email string format
     const TESTER = /\S+@\S+\.\S+/;
     return TESTER.test(email);
   }
 
-  capitalize(text) {
+  CAPITALIZE(text) {
     // Capitalize string
     let capitalize = text.toLocaleLowerCase().split("");
     capitalize[0] = capitalize[0].toLocaleUpperCase();
