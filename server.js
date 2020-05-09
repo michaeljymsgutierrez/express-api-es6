954b4d7c6d42f115fffb68ad3fe3770f5d206a4f
michaeljymsgutierrez <leeez711@gmail.com>
2020-05-09 11:39:02 +0800

    Updated main server script entry point

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-/**
- * Main Express Server
- */
+// Main Express Server
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import ip from "ip";
-import https from "https";
-import fs from "fs";
 import { API } from "./endpoints/test";
-import ENV from "./env";
+import Run from "./run";
 
 const app = express();
 const path = __dirname;
 const publicFolder = path + "/files";
+const RUN = new Run();
 
-/**
- * Middlewares
- */
+// Middlewares
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -26,34 +21,12 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use("/public", express.static(publicFolder));
 
+// Test root endpoint
 app.get("/", (req, res) => {
-	res.json({ success: 200 });
+  res.json({ success: 200 });
 });
 
+// Set here endpoint and API modules
 app.get("/api", API);
 
-if (ENV.mode && ENV.mode === "development") {
-	app.listen(ENV.port, ENV.host, () => {
-		console.log(
-			`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${
-				ENV.host
-			}:${ENV.port}\n  =====================================`
-		);
-	});
-} else if (ENV.mode === "production") {
-	/**
-	 * SSL Directory for production
-	 */
-	const sslOptions = {
-		key: fs.readFileSync(`${ENV.ssl_directory}/key.pem`),
-		cert: fs.readFileSync(`${ENV.ssl_directory}/cert.pem`)
-	};
-
-	https.createServer(sslOptions, app).listen(ENV.port, ENV.host, () => {
-		console.log(
-			`\n  =====================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${
-				ENV.host
-			}:${ENV.port}\n  =====================================`
-		);
-	});
-}
+RUN.server(app);
