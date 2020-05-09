4b3dfc21654312f1e07a97ea21f05811f1d034e8
michaeljymsgutierrez <leeez711@gmail.com>
2020-05-09 11:35:00 +0800

    Moved run script

---
+++ run.js
@@ -0,0 +1,39 @@
+import https from "https";
+import fs from "fs";
+import ENV from "./env";
+
+class Run {
+  server(app) {
+    // Config for running express/node server DEV || PROD
+
+    if (ENV.mode && ENV.mode === "development") {
+      app.listen(ENV.port, ENV.host, () => {
+        console.log(
+          `\n  ====================================================\n  = STATUS : ${ENV.mode.toUpperCase()} MODE\n  = SERVER : http://${
+            ENV.host
+          }:${
+            ENV.port
+          }\n  = STARTED: ${new Date()}  \n  ====================================================`
+        );
+      });
+    } else if (ENV.mode === "production") {
+      // SSL Directory for production
+      const sslOptions = {
+        key: fs.readFileSync(`${ENV.ssl_directory}/key.pem`),
+        cert: fs.readFileSync(`${ENV.ssl_directory}/cert.pem`)
+      };
+
+      https.createServer(sslOptions, app).listen(ENV.port, ENV.host, () => {
+        console.log(
+          `\n  ====================================================\n  = STATUS: ${ENV.mode.toUpperCase()} MODE\n  = SERVER: http://${
+            ENV.host
+          }:${
+            ENV.port
+          }\n  = STARTED: ${new Date()}  \n  ====================================================`
+        );
+      });
+    }
+  }
+}
+
+export default Run;
