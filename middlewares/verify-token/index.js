00ed2296043b5ae754e09fed603136152f2a4f19
michaeljymsgutierrez <leeez711@gmail.com>
2020-06-20 22:51:12 +0800

    Added middlewares directory

---
+++ middlewares/verify-token/index.js
@@ -0,0 +1,25 @@
+// Verify Token
+import jwt from "jsonwebtoken";
+import ENV from "../../env";
+
+const VERIFY_TOKEN = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (authorization) {
+    jwt.verify(authorization, ENV.secret_key, (error, decoded) => {
+      if (error) {
+        res
+          .status(422)
+          .json({ success: false, message: "Invalid or Expired token" });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res
+      .status(422)
+      .json({ success: false, message: "Invalid or Expired token" });
+  }
+};
+
+export default VERIFY_TOKEN;
